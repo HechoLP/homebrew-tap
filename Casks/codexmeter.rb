4c91dd485debe8efc86577e6b2ae9aac61d162db
HechoLP <lrang20101@naver.com>
2026-08-29 14:54:31 +0900

    chore(cask): update CodexMeter to 1.1.0

--- Casks/codexmeter.rb
+++ Casks/codexmeter.rb
@@ -1,6 +1,6 @@
 cask "codexmeter" do
-  version "1.0.6"
-  sha256 "c43f0e8cb8186d3b66402f84174ee1f5fa83bac022c0f1a02691af9a72384cf1"
+  version "1.1.0"
+  sha256 "c14f81150555f245c14a5b6a94e9076ed5ca5d39b9136dde29b26222f6179fa2"
 
   url "https://github.com/HechoLP/CodexMeter/releases/download/v#{version}/CodexMeter-#{version}.zip"
   name "CodexMeter"
